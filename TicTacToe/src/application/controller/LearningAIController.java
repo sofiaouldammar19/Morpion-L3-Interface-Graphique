e4f7013d2383e4910f68d23226ea4d98448e80a6
sofiaouldammar19 <ouldammarsofia@gmail.com>
2024-02-20 12:18:21 +0100

    test

--- TicTacToe/src/application/controller/LearningAIController.java
+++ TicTacToe/src/application/controller/LearningAIController.java
@@ -7,8 +7,13 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import ai.Config;
 import ai.MultiLayerPerceptron;
-import ai.Coup; // Assuming Coup is a class you have defined for your game states
+import ai.Coup;
+import ai.Test;// Assuming Coup is a class you have defined for your game states
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class LearningAIController {
@@ -37,7 +42,7 @@ public class LearningAIController {
             protected Void call() throws Exception {
                 // Simulate loading data and training the model
                 updateMessage("Loading data...");
-                HashMap<Integer, Coup> coups = loadGames("./resources/dataset/Tic_tac_initial_results.csv");
+    			HashMap<Integer, Coup> mapTrain = loadCoupsFromFile("./resources/train_dev_test/train.txt");
                 // Simulated training process
                 final int epochs = 10000; // Total number of training epochs
                 for (int i = 1; i <= epochs; i++) {
@@ -63,9 +68,40 @@ public class LearningAIController {
     }
 
     // Placeholder for your loadGames method
-    private HashMap<Integer, Coup> loadGames(String fileName) {
-        // Implement loading logic here
-        return new HashMap<>();
+    private HashMap<Integer, Coup> loadCoupsFromFile(String file) {
+    	System.out.println("loadCoupsFromFile from "+file+" ...");
+		HashMap<Integer, Coup> map = new HashMap<>();
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(file))));
+			String s = "";
+			while ((s = br.readLine()) != null) {
+				String[] sIn = s.split("\t")[0].split(" ");
+				String[] sOut = s.split("\t")[1].split(" ");
+
+				double[] in = new double[sIn.length];
+				double[] out = new double[sOut.length];
+
+				for (int i = 0; i < sIn.length; i++) {
+					in[i] = Double.parseDouble(sIn[i]);
+				}
+
+				for (int i = 0; i < sOut.length; i++) {
+					out[i] = Double.parseDouble(sOut[i]);
+				}
+
+				Coup c = new Coup(9, "");
+				c.in = in ;
+				c.out = out ;
+				map.put(map.size(), c);
+			}
+			br.close();
+		} 
+		catch (Exception e) {
+			System.out.println("Test.loadCoupsFromFile()");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		return map ;
     }
 
     @FXML
